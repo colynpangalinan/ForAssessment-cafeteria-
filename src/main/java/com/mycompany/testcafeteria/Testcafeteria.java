24b0802ed16fc39c58a227451606b372ecd117b2
trrsj <trrsj@Juliana>
2025-04-28 23:54:04 +0800

    assessmentt

---
+++ src/main/java/com/mycompany/testcafeteria/Testcafeteria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testcafeteria;
+
+/**
+ *
+ * @author trrsj
+ */
+public class Testcafeteria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
